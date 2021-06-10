07cbfe1acea2dd2356cb9d4bc1a1ceb1c0d8d8e6
Romain Cahuet <romain.cahuet@epita.fr>
2021-06-10 11:13:28 +0200

    [ANGULAR][ADD] chatbox: added the chatbox component

--- frontend/button-front/src/app/app.module.ts
+++ frontend/button-front/src/app/app.module.ts
@@ -1,14 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { SocketIoModule, SocketIoConfig } from 'ngx-socket-io';
+
+const config: SocketIoConfig = { url: 'http://localhost:5001', options: {} };
 
 import { AppComponent } from './app.component';
+import { ChatboxComponent } from './chatbox/chatbox.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ChatboxComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    SocketIoModule.forRoot(config),
   ],
   providers: [],
   bootstrap: [AppComponent]
